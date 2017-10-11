bdcb8ab8ed2ab629dda248df8c7704849c791ce7
Сергей <sergey-global@yandex.ru>
2017-10-11 15:01:11 +0300

    initial

--- aisd_lab3/aisd_lab3.cpp
+++ aisd_lab3/aisd_lab3.cpp
@@ -1,11 +1,77 @@
-// aisd_lab3.cpp: определяет точку входа для консольного приложения.
-//
+#include <iostream>
+#include <string>
 
-#include "stdafx.h"
+template <typename T>
+struct Node {
+	int key;
+	T data;
+	Node<T> *left, *right;
+	Node() {
+		key = 0;
+		left = nullptr;
+		right = nullptr;
+	}
+	Node(int k, T d) {
+		key = k;
+		data = d;
+		left = nullptr;
+		right = nullptr;
+	}
+	Node(int k, T d, Node* l, Node* r) {
+		key = k;
+		data = d;
+		left = l;
+		right = r;
+	}
+};
 
+template <typename T>
+class Tree {
+public:
+	Node<T> root;
+	int size;
+	Tree() {
+		Node<T> * root = new Node<T>;
+		size = 0;
+	}
+	/*~Tree() {
+
+	}*/
+	void add(int key, T data) {
+		Node<T> * n = new Node<T>;
+		*n = { key, data };
+		if (!size) {
+			root = n;
+		}
+		else {
+			bool useRight = false;
+			Node<T> *next = root;
+			while (next != nullptr)
+			{
+				curr = next;
+				if (key >= curr->key) {
+					next = curr->right;
+					useRight = false;
+				}
+					
+				else {
+					next = curr->left;
+					useRight = true;
+				}
+					
+			}
+			if (useRight) curr->right = n;
+			else curr->left = n;
+
+			
+		}
+	}
+};
 
 int main()
 {
+	Tree<std::string> t;
+	t.add(1, "hui");
     return 0;
 }
 
